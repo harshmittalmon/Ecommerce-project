11484fb275a978fff6786b1acb21ea4c81861341
HARSH MITTAL <harshmittalmon@gmail.com>
2025-05-03 21:00:32 +0530

    just some updates

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
+    // console.log("Mongo URI:", process.env.MONGO_URI);
+
     console.log(`Successfully connnected to mongoDB 👍`);
   } catch (error) {
     console.error(`ERROR: ${error.message}`);
